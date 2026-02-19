91e09484bbd8e5bbe2f53b93c270115890aba7fd
Chyi Wang <chyiyenwang@gmail.com>
2026-02-19 10:00:44 -0500

    added new brand logo

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,14 +1,33 @@
 import Link from "next/link";
+import Image from "next/image";
 import SearchBar from "../search-bar/search-bar";
 import styles from "./header.module.css";
 
 export default function Header() {
   return (
     <div className={styles.header}>
-      <h1 className={styles.logo}>Arc Raiders</h1>
+      <span className={styles.brand}>
+        <a href="/">
+          <span className={styles["logo-wrapper"]}>
+            <Image
+              className={styles.logo}
+              src="/arc-raiders-logo.webp"
+              alt="Arc Raiders Logo"
+              width={50}
+              height={50}
+            />
+          </span>
+          <h3 className={styles.title}>
+            <div>Arc</div>
+            <div>Raiders</div>
+          </h3>
+        </a>
+      </span>
+
       <span className={styles.searchbar}>
         <SearchBar />
       </span>
+
       <span className={styles.navigation}>
         <Link href="/items">
           <h2>Items</h2>
