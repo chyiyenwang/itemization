36d13dbd94df84de2274630cd18e7103c0f9198d
Chyi Wang <chyiyenwang@gmail.com>
2026-02-26 11:25:47 -0500

    changing to atomic query

--- app/utils/dataFromApi.ts
+++ app/utils/dataFromApi.ts
@@ -1,5 +1,29 @@
 import { prisma } from "../lib/prisma";
 
+function mapCreateItemData(item: any) {
+  return {
+    id: item.id,
+    name: item.name,
+    description: item.description,
+    item_type: item.item_type,
+    icon: item.icon,
+    rarity: item.rarity,
+    value: item.value,
+    workbench: item.workbench,
+    stat_block: {
+      create: { ...item.stat_block },
+    },
+    flavor_text: item.flavor_text,
+    subcategory: item.subcategory,
+    created_at_api: item.created_at,
+    updated_at_api: item.updated_at,
+    shield_type: item.shield_type,
+    loot_area: item.loot_area,
+    sources: item.sources,
+    ammo_type: item.ammo_type,
+  };
+}
+
 function mapUpsertItemData(item: any) {
   return {
     id: item.id,
@@ -28,92 +52,70 @@ function mapUpsertItemData(item: any) {
   };
 }
 
-function mapCreateItemData(item: any) {
+function mapCreateComponentData(data: any[]) {
   return {
-    id: item.id,
-    name: item.name,
-    description: item.description,
-    item_type: item.item_type,
-    icon: item.icon,
-    rarity: item.rarity,
-    value: item.value,
-    workbench: item.workbench,
-    stat_block: {
-      create: { ...item.stat_block },
+    createMany: {
+      data: data.map((c) => {
+        const compId = c.item?.id ?? c.component.id;
+        return {
+          component_id: compId,
+          quantity: c.quantity,
+        };
+      }),
     },
-    flavor_text: item.flavor_text,
-    subcategory: item.subcategory,
-    created_at_api: item.created_at,
-    updated_at_api: item.updated_at,
-    shield_type: item.shield_type,
-    loot_area: item.loot_area,
-    sources: item.sources,
-    ammo_type: item.ammo_type,
   };
 }
 
-async function mapComponentData(
-  tx: any,
-  id: string,
-  ApiDataItem: any[],
-  table: any,
-) {
-  for (const comp of ApiDataItem ?? []) {
-    await tx[table].upsert({
-      where: {
-        item_id_component_id: {
-          item_id: id,
-          component_id: comp.item?.id ?? comp.component.id,
+function mapDeleteAndUpsertComponentData(id: string, data: any[]) {
+  return {
+    deleteMany: {
+      component_id: {
+        notIn: data.map((c) => c.item?.id ?? c.component.id),
+      },
+    },
+    upsert: data.map((comp) => {
+      const compId = comp.item?.id ?? comp.component.id;
+      return {
+        where: {
+          item_id_component_id: {
+            item_id: id,
+            component_id: compId,
+          },
         },
-      },
-      update: {
-        item_id: id,
-        component_id: comp.item?.id ?? comp.component.id,
-        quantity: comp.quantity,
-      },
-      create: {
-        item_id: id,
-        component_id: comp.item?.id ?? comp.component.id,
-        quantity: comp.quantity,
-      },
-    });
-  }
+        update: { quantity: comp.quantity },
+        create: {
+          component_id: compId,
+          quantity: comp.quantity,
+        },
+      };
+    }),
+  };
 }
 
 export default async function dataFromApi(ApiDataItem: any) {
-  console.log(ApiDataItem.id);
   try {
-    await prisma.$transaction(async (tx) => {
-      await tx.item.upsert({
-        where: { id: ApiDataItem.id },
-        update: mapUpsertItemData(ApiDataItem),
-        create: mapCreateItemData(ApiDataItem),
-      });
-
-      await mapComponentData(
-        tx,
-        ApiDataItem.id,
-        ApiDataItem.components,
-        "itemComponent",
-      );
-      await mapComponentData(
-        tx,
-        ApiDataItem.id,
-        ApiDataItem.recycle_components,
-        "itemRecycleComponent",
-      );
-      await mapComponentData(
-        tx,
-        ApiDataItem.id,
-        ApiDataItem.recycle_from,
-        "itemRecycleFrom",
-      );
-      await mapComponentData(
-        tx,
-        ApiDataItem.id,
-        ApiDataItem.used_in,
-        "itemUsedIn",
-      );
+    const { id, used_in, recycle_from, recycle_components, components } =
+      ApiDataItem;
+    console.log("inserting...", id);
+    await prisma.item.upsert({
+      where: { id },
+      create: {
+        ...mapCreateItemData(ApiDataItem),
+        used_in: mapCreateComponentData(used_in),
+        recycle_from: mapCreateComponentData(recycle_from),
+        recycle_components: mapCreateComponentData(recycle_components),
+        components: mapCreateComponentData(components),
+      },
+      update: {
+        ...mapUpsertItemData(ApiDataItem),
+        used_in: mapDeleteAndUpsertComponentData(id, used_in),
+        recycle_from: mapDeleteAndUpsertComponentData(id, recycle_from),
+        recycle_components: mapDeleteAndUpsertComponentData(
+          id,
+          recycle_components,
+        ),
+        components: mapDeleteAndUpsertComponentData(id, components),
+      },
     });
     console.log(`Successfully inserted ${ApiDataItem.id}`);
   } catch (e) {
