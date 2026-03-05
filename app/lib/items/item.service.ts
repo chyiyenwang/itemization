b61d2e0c405cb8b1dc09736bb79a27825f6f6145
Chyi Wang <chyiyenwang@gmail.com>
2026-03-05 13:00:53 -0500

    moved stuff into helper

--- app/lib/items/item.service.ts
+++ app/lib/items/item.service.ts
@@ -3,37 +3,16 @@ import { BaseItemSchema } from "./item.schema";
 import { Item } from "@/app/types";
 import { findUniqueItem, upsertItemRecord } from "./item.repository";
 import { mapApiItemToDomain } from "./item.mapper";
+import { getRequiredStatBlock, isStale } from "./item.helpers";
 
 import * as items from "@/app/data";
-
-// TODO: fix the any types in this file, especially the type assertion in getItem function. Maybe we can add a separate type for the API response and validate against that instead of the internal Item type which has some differences (e.g. statBlock can be null from API but is required in our internal type)
-function getRequiredStatBlock(stat: Item["statBlock"] | null) {
-  return (
-    stat ?? {
-      weight: null,
-      stackSize: null,
-    }
-  );
-}
-
-const STALE_TIME_DAYS = 7;
-function isStale(lastFetched: Date) {
-  const currentDate = new Date();
-  currentDate.setDate(currentDate.getDate() - STALE_TIME_DAYS);
-
-  return lastFetched < currentDate ? true : false;
-}
 
 export async function getItem(id: string): Promise<Item | null> {
   let item = await findUniqueItem(id);
 
   if (!item || !item.statBlock || isStale(item.lastFetched)) {
-    // real API call to fetch item data, then upsert into DB
     const itemFromApi = await fetchApiItem(id);
     if (!itemFromApi) return null;
-
-    // fake API call - find item from local data
-    // const itemFromApi = Object.values(items).find((item) => item.id === id);
 
     const validatedItem = validateApiItem(itemFromApi);
     if (!validatedItem) {
@@ -78,6 +57,9 @@ export default async function upsertItem(ApiDataItem: Item) {
 }
 
 async function fetchApiItem(id: string): Promise<any | null> {
+  // fake API call - find item from local data
+  // return Object.values(items).find((item) => item.id === id);
+
   const res = await fetch(
     `https://metaforge.app/api/arc-raiders/items?id=${id}&includeComponents=true`,
     {
