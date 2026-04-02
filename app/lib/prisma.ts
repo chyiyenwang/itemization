fd1481ddb4bf33cd00f0438364a580167436353d
Chyi Wang <chyiyenwang@gmail.com>
2026-04-02 14:35:02 -0700

    using a connection pool instead of a string

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -2,8 +2,14 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaPg({ connectionString });
+const pool = {
+  connectionString: process.env.DATABASE_URL,
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+};
+
+const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({
   adapter,
